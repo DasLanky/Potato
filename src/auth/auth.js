30c3f834949ac50ede0c802fdde6d1b6696749b7
Langston Chandler <lrchandl@uci.edu>
2016-07-16 17:04:12 -0700

    Implemented database lookup for authentication

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -2,6 +2,28 @@ var db = require('./db/auth-db.js');
 
 module.exports = {
     verify: function(req) {
-        return true;
+        if (!("user" in req) || !("pass" in req)) {
+            return false;
+        }
+        if (db.isMatch(req.user, req.pass)) {
+            return true;
+        }
+        else {
+            //Username/password combination invalid
+            return false;
+        }
+    }
+
+    verifyAdmin: function(req) {
+        if (!("user" in req) || !("pass" in req)) {
+            return false;
+        }
+        if (db.isAdmin(req.user, req.pass)) {
+            return true;
+        }
+        else {
+            //Username/password combination invalid
+            return false;
+        }
     }
 };
--- src/auth/db/auth-db.js
+++ src/auth/db/auth-db.js
@@ -1,5 +1,12 @@
-const fs = require('fs');
+const data = require('./db.json');
 
 module.exports = {
+    isAdmin: function(user, pass) {
+        return data.adminUserName == user && data.adminPassword == pass;
+    },
 
+    isMatch: function(user, pass) {
+        return (isAdmin(user, pass)
+             || data.users.guests.indexOf({ "user" : user, "pass" : pass}) != -1);
+    }
 };
