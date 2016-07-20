defd5ef5a433124878518502fc3efee501895ab1
Langston Chandler <lrchandl@uci.edu>
2016-07-19 19:25:58 -0700

    Completely redid authentication system to use express-session

--- bin/potato-init.js
+++ bin/potato-init.js
@@ -1,82 +1,98 @@
 const express = require('express')
     , pug = require('pug')
-    , morgan = require('morgan')
     , nib = require('nib')
-    , session = require('express-session');
+    , session = require('express-session')
+    , bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser');
 
-//Custom modules
-var potatoProperties = require('../potato.json');
-var potatoAuth = require('../src/auth/auth.js');
-
-console.log(potatoProperties);
-console.log(potatoAuth);
-
-//Middleware init
-morgan('combined', {
-    skip: function(req, res) { return res.statusCode < 400 }
-});
+var properties = require('../potato.json');
+var auth = require('../src/auth.js');
 
 //Create applet
 var app = express();
+
+app.use(bodyParser()); //Parse JS forms into req.body
+app.set('view engine', 'pug'); //HTML generator engine is pug (Jade)
+
+app.use(cookieParser()); //Cookie-parser for authentication
+
+app.use(session({
+    secret: 'potato',
+    cookie: {
+        maxAge: (new Date()).getTime(),
+        token: "blank"
+    }
+}));
 
 //Serve files within the web directory
 app.set('views', __dirname + '/../web');
 app.use(express.static(__dirname + '/../web'));
 
-//Use session middleware
-app.use(session({
-    secret: potatoProperties.sessionSecret,
-    cookie: { maxAge: potatoProperties.timeout }
-}));
+function verifySession(req, res) {
+    if (!auth.verify(req)) {
+        res.redirect('/login');
+        return;
+    }
+}
 
-//HTML generator engine is pug (Jade)
-app.set('view engine', 'pug');
+app.get('/',
+    function(req, res) {
+    verifySession(req, res);
+    res.render('index', {
+        title: 'Potato',
+        message: 'Potato Homepage',
+        user: auth.getName(req)
+    });
+});
 
-app.get('/', function(req,res) {
-    //console.log(req); //Super verbose
+app.get('/login',
+    function(req,res) {
+    res.render('login', {
+        title: 'Potato',
+        message: 'Potato Homepage',
+    });
+});
 
-    if (potatoAuth.verify(req)) {
+app.post('/login', function(req, res) {
+    console.log(req.cookies);
+    console.log("User login with name " + req.body.name
+              + " and password " + req.body.pass);
+    if (!auth.hasUser(req.body.name)) {
+        console.log("\tUser not found");
+        res.redirect('/login');
+        return;
+    }
+    else if (!auth.checkUser(req.body.name, req.body.pass)){
+        console.log("\tIncorrect password");
+        res.redirect('/login');
+        return;
+    }
+    else {
+        auth.register(req);
         res.render('index', {
             title: 'Potato',
             message: 'Potato Homepage',
-            user: req.user,
-        });
-    }
-    else {
-        res.render('login', {
-            title: 'Potato Login',
-            message: 'Potato Login'
+            user: req.body.name
         });
     }
 });
 
-app.get('/login/:user/:pass', function(req, res) {
-    req.user = req.params.user;
-    req.pass = req.params.pass;
-    console.log('User attempted to login with name '
-                + req.user
-                + ' and password '
-                + req.pass);
-    if (potatoAuth.verify(req)) {
-        console.log('\tAuthorized\n');
-        res.render('index', {
-            title: 'Potato',
-            message: 'Potato Homepage',
-            user: req.user
-        });
-    }
-    else {
-        console.log('\tDenied\n');
-        res.render('login', {
-            title: 'Potato Login',
-            message: 'Potato Login'
-        });
-    }
+app.all('/logout',function(req, res) {
+    req.session.destroy();
+    res.end();
 })
 
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+
+app.get('/loginSuccess', function(req, res, next) {
+  res.send('Successfully authenticated');
+});
+
 //Listen for incoming connections
-app.listen(potatoProperties.port, function() {
+app.listen(properties.port, function() {
     console.log("Test webserver listening on http://<HostAddress>:"
-                + potatoProperties.port
+                + properties.port
                 + "\n");
 });
---
+++ src/auth.js
@@ -0,0 +1,84 @@
+const _ = require('underscore')
+   , crypto = require('crypto')
+   , properties = require('../potato.json')
+   , fs = require('fs')
+   , cookieParser = require('cookie-parser');
+var users = require(properties.userdb);
+var tokens = require(properties.tokendb);
+
+const NUM_BYTES = 32;
+
+function hasToken(token) {
+    for (tokenIndex in tokens) {
+        if (tokens[tokenIndex].token == token) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function newToken(_name, _token) {
+    tokens.push({
+        token: _token,
+        name: _name
+    });
+}
+
+module.exports = {
+    newToken: newToken,
+    hasToken: hasToken,
+
+    export: function() {
+        //Write out to userdb and tokendb
+    },
+
+    hasUser: function(name) {
+        var type, user;
+        for (typeName in users) {
+            type = users[typeName];
+            for (userName in type) {
+                user = type[userName];
+                if (user.name == name) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    checkUser: function(name, pass) {
+        var type, user;
+        for (typeName in users) {
+            type = users[typeName];
+            for (userName in type) {
+                user = type[userName];
+                console.log(user);
+                if (user.name == name) {
+                    if (user.pass == pass) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+        return false;
+    },
+
+    getName: function(request) {
+        var token;
+        for (tokenName in tokens) {
+            token = tokens[tokenName];
+            if (token.token == request.session.cookie['connect.sid']) {
+                return token.name;
+            }
+        }
+    },
+
+    register: function(request) {
+        newToken(request.body.name, request.session.cookie['connect.sid']);
+    },
+
+    verify: function(request) {
+        return hasToken(request.session.cookie['connect.sid']);
+    }
+}
